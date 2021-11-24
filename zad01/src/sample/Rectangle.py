1b36d54d7302e4f448e0b290a8d5ea9f47e26b38
Adrian Stokwisz <astokwisz@INF.UG.EDU.PL>
2021-11-24 09:53:27 +0100

    zad01

---
+++ zad01/src/sample/Rectangle.py
@@ -0,0 +1,15 @@
+class Rect:
+    def draw_rect(self, a, b):
+        if isinstance(a, int) and isinstance(b, int):
+            if a > 0 and b > 0:
+                r = ""
+                r += "*" * b + "\n"
+                for i in range(0, a - 2):
+                    r += ("*" + " " * (b - 2) + "*") + "\n"
+                if a > 1:
+                    r += "*" * b + "\n"
+                return r
+            else:
+                raise Exception("Długość boku musi być dłuższa od 0")
+        else:
+            raise Exception("Długość boku musi być liczbą całkowitą")
---
+++ zad01/tests/RectangleParameterizedFile.py
@@ -0,0 +1,20 @@
+import unittest
+from sample.Rectangle import *
+
+class RectangleParameterizedFile(unittest.TestCase):
+
+    def test_from_file(self):
+      fileTest = open("data/rectangle_data_test")
+      tmpRectangle = Rect()
+      for line in fileTest:
+        if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+            continue
+        else:
+            data = line.split(",")
+            inp1, inp2, result = int(data[0]) | bool(data[0]), int(data[1]) | bool(data[1]), data[2].rstrip('\n').encode("idna")
+            self.assertEqual(tmpRectangle.draw_rect(inp1, inp2).encode("unicode_escape"), result)
+      fileTest.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
